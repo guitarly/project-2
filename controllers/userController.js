8764b43956c9edacbd2caa64ddae7ae7d3c35df2
025609 <matthewnguyen@C1MRQ046HF4H.fios-router.home>
2017-04-16 17:56:34 -0400

    check gitignore file

--- controllers/userController.js
+++ controllers/userController.js
@@ -8,10 +8,20 @@ router.get('/register', function(req, res) {
     res.render('register');
 });
 
+router.post("/register", function(req, res) {
+    console.log(req.body);
+})
+
 // Login
 router.get('/login', function(req, res) {
     res.render('login');
 });
+
+
+router.post("/login", function(req, res) {
+    console.log("I am here.. just submit login");
+});
+
 // Dashboard
 router.get('/index', function(req, res) {
     res.render('index');
