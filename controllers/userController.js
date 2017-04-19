a1574e577dbd66b3a404ed9e0db512ea6feaee78
025609 <matthewnguyen@C1MRQ046HF4H.fios-router.home>
2017-04-19 07:52:54 -0400

    fixed - session timeout

--- controllers/userController.js
+++ controllers/userController.js
@@ -87,7 +87,7 @@ function ensureAuthenticated(req, res, next) {
         return next();
     } else {
         req.flash('error_msg', 'You are not logged in');
-        res.redirect('/login');
+        res.redirect('/');
     }
 }
 
